4bfc09a3107eb6ba6300a68c4955303eb6b9d4a7
snakhale <samir.nakhale@gmail.com>
2022-09-11 19:18:03 +0530

    added sysout statement

--- src/main/java/com/javatechie/app/git_demo1/App.java
+++ src/main/java/com/javatechie/app/git_demo1/App.java
@@ -1,5 +1,7 @@
 package com.javatechie.app.git_demo1;
 
+import com.javatechie.app.git_demo1.Constant;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,16 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Added new line for git demo");
+        
+       
+    }
+    
+    public void addData(String data) {
+    	System.out.println("data ="+data);
+    	if (data.equals(Constant.DATA_TYPE)) {
+    		System.out.println("I am Samir");
+    	}
+    	
     }
 }
